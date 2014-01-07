7fe0c488d367a6af71f631d05edd74431724b78e
noxan <noxan@byteweaver.net>
2014-01-07 19:25:16 +0100

    create empty index template and register a template view for it

--- evolin/urls.py
+++ evolin/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.views.generic import TemplateView
 
 from evolin.api import api_v1
 
@@ -7,6 +8,7 @@ from evolin.api import api_v1
 admin.autodiscover()
 
 urlpatterns = patterns('',
+    url(r'^$', TemplateView.as_view(template_name='evolin/index.html'), name='index'),
     url(r'^api/', include(api_v1.urls)),
 
     url(r'^admin/', include(admin.site.urls)),
