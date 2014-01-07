16a756034e894f143828e82a910dcce3e7a79e2d
noxan <noxan@byteweaver.net>
2014-01-07 17:53:08 +0100

    add basic project model definition

--- evolin/projects/models.py
+++ evolin/projects/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Project(models.Model):
+    name = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL)
+
+    def __str__(self):
+        return self.full_name
