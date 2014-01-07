11cf0573ba97c8e21218eccd2302d4504fb5daa9
noxan <noxan@byteweaver.net>
2014-01-07 17:57:02 +0100

    add an issue state model and reference it from issue model

--- evolin/issues/admin.py
+++ evolin/issues/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 
-from evolin.issues.models import Issue
+from evolin.issues.models import Issue, IssueState
 
 
 class IssueAdmin(admin.ModelAdmin):
-    list_display = ['number', 'title', 'project']
+    list_display = ['number', 'title', 'project', 'state']
 
 
 admin.site.register(Issue, IssueAdmin)
+admin.site.register(IssueState)
--- evolin/issues/models.py
+++ evolin/issues/models.py
@@ -3,10 +3,18 @@ from django.db import models
 from evolin.projects.models import Project
 
 
+class IssueState(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return '%s' % self.name
+
+
 class Issue(models.Model):
     number = models.PositiveIntegerField()
     title = models.CharField(max_length=255)
     body = models.TextField(blank=True, default='')
+    state = models.ForeignKey(IssueState)
     project = models.ForeignKey(Project)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
