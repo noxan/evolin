7c20b3350f2ac283ca237617582478624684bb93
noxan <noxan@byteweaver.net>
2014-01-07 17:53:50 +0100

    add basic issue model definition

--- evolin/issues/models.py
+++ evolin/issues/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+from evolin.projects.models import Project
+
+
+class Issue(models.Model):
+    number = models.PositiveIntegerField()
+    title = models.CharField(max_length=255)
+    body = models.TextField(blank=True, default='')
+    project = models.ForeignKey(Project)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return '#%d - %s' % (self.number, self.title)
