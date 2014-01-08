60a9652911dc354bced5990c122d77f26d3839e4
noxan <noxan@byteweaver.net>
2014-01-08 18:42:11 +0100

    wip

--- evolin/api.py
+++ evolin/api.py
@@ -1,8 +1,10 @@
 from tastypie.api import Api
 
 from evolin.issues.api import IssueResource, IssueStateResource
+from evolin.projects.api import ProjectResource
 
 
 api_v1 = Api(api_name='v1')
 api_v1.register(IssueResource())
 api_v1.register(IssueStateResource())
+api_v1.register(ProjectResource())
--- manage.py
+++ manage.py
@@ -3,6 +3,12 @@ import os
 import sys
 
 if __name__ == "__main__":
+    target = 'development'
+    if os.path.isfile('.target'):
+        target = open('.target', 'r').readline().strip()
+        if not target or not os.path.isfile('casevo/settings/%s.py' % target):
+            sys.exit("Unknown target '%s'" % target)
+
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "evolin.settings")
 
     from django.core.management import execute_from_command_line
