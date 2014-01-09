ae164adc0ad9919ec994e62048d887f5a9c2203d
noxan <noxan@byteweaver.net>
2014-01-09 11:55:12 +0100

    add logging to github issue importer

--- evolin/issues/github.py
+++ evolin/issues/github.py
@@ -1,3 +1,5 @@
+import logging
+
 from github import Github
 
 from django.conf import settings
@@ -5,6 +7,9 @@ from django.conf import settings
 from evolin.issues.settings import ISSUES_GITHUB_STATE_MAPPING
 from evolin.issues.models import Issue, IssueState
 from evolin.projects.models import Project
+
+
+logger = logging.getLogger(__name__)
 
 
 def sync_issues(github_project_name):
@@ -16,6 +21,10 @@ def sync_issues(github_project_name):
 
     project = Project.objects.get(owner__username=owner, name=name)
 
+    logger.info("Importing all new issues for project '%s'." % project.full_name)
+
+    project_import_count = 0
+
     for state in ISSUES_GITHUB_STATE_MAPPING.keys():
         for github_issue in repo.get_issues(state=state):
             issuestate = IssueState.objects.get(pk=ISSUES_GITHUB_STATE_MAPPING[state])
@@ -26,4 +35,10 @@ def sync_issues(github_project_name):
                 'updated_at': github_issue.updated_at,
                 'state': issuestate,
             })
-            print(created, issue)
+            if created:
+                logger.info("Imported new issue '%s'." % (issue))
+                project_import_count += 1
+            else:
+                logger.debug("Skipped issue '%s' because it has already been imported." % (issue))
+
+    logger.info("Imported %d new issues for project '%s'." % (project_import_count, project.full_name))
